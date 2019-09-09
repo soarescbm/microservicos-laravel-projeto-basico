355ea6333732ee9eed70ffc31e3a1b7cf7adaa56
Paulo Soares <soarescbm@gmail.com>
2019-09-09 07:49:53 -0300

    cloudbuild

--- tests/Feature/Http/Controllers/Api/CategoryControllerTest.php
+++ tests/Feature/Http/Controllers/Api/CategoryControllerTest.php
@@ -76,4 +76,71 @@ class CategoryControllerTest extends TestCase
             ->assertJsonValidationErrors(['name', 'is_active'])
             ->assertJsonFragment([\Lang::trans('validation.boolean', ['attribute' => 'is active'])]);
     }
+
+    public function testStore()
+    {
+        $response = $this->json('post', route('categories.store'), [
+            'name' => 'test'
+        ]);
+
+        $id = $response->json('id');
+        $category = Category::find($id);
+
+        $response->assertStatus(201)
+            ->assertJson($category->toArray());
+        $this->assertTrue($response->json('is_active'));
+        $this->assertNull($response->json('description'));
+
+
+        $response = $this->json('post', route('categories.store'), [
+            'name' => 'test',
+            'description' => 'description',
+            'is_active' => false
+        ]);
+
+
+        $response->assertJsonFragment([
+            'description' => 'description',
+            'is_active' => false,
+        ]);
+
+    }
+
+    public function testUpdate()
+    {
+        $category = factory(Category::class)->create([
+            'description' => 'description',
+            'is_active' => false
+        ]);
+        $response = $this->json('put', route('categories.update',  ['category' => $category->id]), [
+            'name' => 'test',
+            'description' => 'test',
+            'is_active' => true
+        ]);
+
+        $id = $response->json('id');
+        $category = Category::find($id);
+
+        $response->assertStatus(200)
+            ->assertJson($category->toArray())
+        ->assertJsonFragment([
+            'description' => 'test',
+            'is_active' => true,
+        ]);
+
+        $response = $this->json('put', route('categories.update',  ['category' => $category->id]), [
+            'name' => 'test',
+            'description' => '',
+        ]);
+
+
+        $response->assertStatus(200)
+            ->assertJsonFragment([
+                'description' => null,
+            ]);
+
+
+
+
+    }
 }
